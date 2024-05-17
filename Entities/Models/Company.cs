b7225e8f9d5b80fb4ee0ce286e06019176bf5799
Taras Karpuk <taras.karpuk@gmail.com>
2024-05-17 14:19:44 -0600

    add Models: Company, Employee

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities.Models
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Country { get; set; }
+
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Position { get; set; }
+
+        public Guid CompanyId { get; set; }
+        public Company? Company { get; set; }
+    }
+}
