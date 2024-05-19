74639254208cd38980fd3c076e4a75e7dfeb260f
Taras Karpuk <taras.karpuk@gmail.com>
2024-05-19 13:58:45 -0600

    regester RepositoryContext

--- CompanyEmployees/Extentions/ServiceExtentions.cs
+++ CompanyEmployees/Extentions/ServiceExtentions.cs
@@ -3,6 +3,7 @@ using LoggerService;
 using Repository;
 using Service.Contracts;
 using Service;
+using Microsoft.EntityFrameworkCore;
 
 namespace CompanyEmployees.Extentions
 {
@@ -35,6 +36,12 @@ namespace CompanyEmployees.Extentions
         public static void ConfigureServiceManager(this IServiceCollection services)
         {
             services.AddScoped<IServiceManager, ServiceManager>();
+        }
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(opts => 
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
         }
     }
 }
