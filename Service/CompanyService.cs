0b7dfea2e88b2b4e7dff262fcdc1931665ca17f4
Taras Karpuk <taras.karpuk@gmail.com>
2024-05-20 21:48:42 -0600

    GetAllCompanies implemented

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Entities.Models;
 using Service.Contracts;
 
 namespace Service
@@ -6,12 +7,26 @@ namespace Service
     internal sealed class CompanyService : ICompanyService
     {
         private readonly IRepositoryManager _repository;
-        private readonly ILoggerManager _loggerManager;
+        private readonly ILoggerManager _logger;
 
         public CompanyService(IRepositoryManager repository, ILoggerManager loggerManager)
         {
             _repository = repository;
-            _loggerManager = loggerManager;
+            _logger = loggerManager;
+        }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        {
+            try
+            {
+                var companies = _repository.Company.GetAllCompanies(trackChanges);
+                return companies;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)} service method {ex}.");
+                throw;
+            }
         }
     }
 }
